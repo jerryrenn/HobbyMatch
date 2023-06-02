de0a1e858c040b3a8dbea173d28c7e58b0591b75
Jerry Renn <jerryrenn1998@gmail.com>
2023-06-02 13:11:23 -0700

    add functional refresh button

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -6,6 +6,9 @@ const Form = () => {
   const [budget, setBudget] = useState("");
   const [activityLevel, setActivityLevel] = useState("");
   const [availableTime, setAvailableTime] = useState("");
+  const [indoorOutdoor, setIndoorOutdoor] = useState("");
+  const [participants, setParticipants] = useState("");
+  const [type, setType] = useState("");
   const [response, setResponse] = useState("");
 
 
@@ -16,12 +19,18 @@ const Form = () => {
       budget,
       activityLevel,
       availableTime,
+      indoorOutdoor,
+      participants,
+      type
     };
 
     axios.post("http://localhost:3000/hobby", userPreferences).then((response) => {
-      setBudget("");
-      setActivityLevel("");
-      setAvailableTime("");
+      // setBudget("");
+      // setActivityLevel("");
+      // setAvailableTime("");
+      // setIndoorOutdoor("");
+      // setParticipants("");
+      // setType("");
       const result = response.data.result;
       console.log('this is the entire text: ', result);
       const paragraphs = result.split('\n\n');
@@ -37,6 +46,41 @@ const Form = () => {
     });
   };
 
+  const handleRegenClick = (e) => {
+    e.preventDefault();
+    setResponse("");
+
+    const userPreferences = {
+      budget,
+      activityLevel,
+      availableTime,
+      indoorOutdoor,
+      participants,
+      type
+    };
+
+    axios.post("http://localhost:3000/hobby", userPreferences).then((response) => {
+      // setBudget("");
+      // setActivityLevel("");
+      // setAvailableTime("");
+      // setIndoorOutdoor("");
+      // setParticipants("");
+      // setType("");
+      const result = response.data.result;
+      console.log('this is the entire text: ', result);
+      const paragraphs = result.split('\n\n');
+      const title = paragraphs[0];
+      const firstParagraph = paragraphs[1];
+      const secondParagraph = paragraphs[2];
+      console.log('should be an array split, 3: ', paragraphs);
+
+      setResponse({ title, firstParagraph, secondParagraph });
+    })
+    .catch((error) => {
+      console.error("Error registering attendee:", error);
+    });
+  }
+
   return (
     <div>
       <h2>Choose Hobby Preferences</h2>
@@ -46,10 +90,10 @@ const Form = () => {
           <select required value={budget} onChange={(e) => setBudget(e.target.value)}>
             <option value="">Select a budget</option>
             <option value="0">$0</option>
-            <option value="10">$10</option>
-            <option value="25">$25</option>
             <option value="50">$50</option>
             <option value="100">$100</option>
+            <option value="200">$200</option>
+            <option value="500">$500</option>
           </select>
         </label>
         <label>
@@ -68,11 +112,42 @@ const Form = () => {
           <select required value={availableTime} onChange={(e) => setAvailableTime(e.target.value)}>
             <option value="">Select your available time</option>
             <option value="Less than 1 hour">Less than 1 hour</option>
-            <option value="1-2 hours">1-2 hours</option>
-            <option value="2-3 hours">2-3 hours</option>
-            <option value="3-4 hours">3-4 hours</option>
-            <option value="4-5 hours">4-5 hours</option>
-            <option value="More than 5 hours">More than 5 hours</option>
+            <option value="1-5 hours">1-5 hours</option>
+            <option value="5-10 hours">5-10 hours</option>
+            <option value="15-20 hours">15-20 hours</option>
+            <option value="20+ hours">20+ hours</option>
+          </select>
+        </label>
+
+        <label>
+          Indoor/Outdoor:
+          <select required value={indoorOutdoor} onChange={(e) => setIndoorOutdoor(e.target.value)}>
+            <option value="">Select your preference</option>
+            <option value="Indoor">Indoor</option>
+            <option value="Outdoor">Outdoor</option>
+            <option value="indoor or outdoor">Both!</option>
+          </select>
+        </label>
+
+        <label>
+          Participants:
+          <select required value={participants} onChange={(e) => setParticipants(e.target.value)}>
+            <option value="">Select your preference</option>
+            <option value="Solo">Solo</option>
+            <option value="2-3">2-3</option>
+            <option value="3-4">3-4</option>
+            <option value="4+">4+</option>
+          </select>
+        </label>
+
+        <label>
+          Adventure/Relaxing:
+          <select required value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="">Select your preference</option>
+            <option value="Relaxing">Relaxing</option>
+            <option value="Moderate">Moderate</option>
+            <option value="Adventurous">Adventurous</option>
+            <option value="relaxing or adventurous">Surprise me</option>
           </select>
         </label>
 
@@ -86,6 +161,7 @@ const Form = () => {
         <strong>{response.title}</strong>
         <p>{response.firstParagraph}</p>
         <p>{response.secondParagraph}</p>
+        <button onClick={handleRegenClick}>Regenerate</button>
         </div>
       )}
 
--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -13,13 +13,13 @@ module.exports = {
 
   getHobby: async (req, res) => {
     console.log('getHobby initiated...');
-    const { budget, activityLevel, availableTime} = req.body;
+    const { budget, activityLevel, availableTime, indoorOutdoor, participants, type} = req.body;
     try {
       const completion = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
           { role: 'system', content: 'You are an article writer, that generates hobbies based on user preferences' },
-          { role: 'user', content: `I have a budget of ${budget} & "${availableTime} a week. I am ${activityLevel}. Please generate me a random hobby. Start your response with just the hobby as a title, wrapped in * to indicate it should be bold. In a paragraph below, give a 3 sentence introduction of the hobby. In another paragraph, write me 3 sentences on how to get started with the hobby. There should be 3 line separations, please include \n for the line separations."` },
+          { role: 'user', content: `I have a budget of ${budget} & "${availableTime} a week. I am ${activityLevel}. Please generate me a random hobby based on my preferences. I want the recommendation to be an ${indoorOutdoor} activity. The activity should include ${participants} participants and should be ${type} Start your response the hobby as a title with a colon after it. In a paragraph below, give a 4 sentence introduction of the hobby. In another paragraph, write me 4 sentences on how to get started with the hobby. There should be 3 line separations, please include \n for the line separations."` },
         ],
         temperature: 0.6,
         max_tokens: 200,
