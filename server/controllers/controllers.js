6cc47b6d8158f2979f866a665afced5834708512
Jerry Renn <jerryrenn1998@gmail.com>
2023-06-01 22:11:56 -0700

    add .env, confirm working davinci

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const { OpenAIApi, Configuration } = require('openai');
 
 const configuration = new Configuration({
-  organization: 'org-JJSozU3BIZWcitrWaAKfRcbl',
-  apiKey: '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',
+  organization: process.env.ORGANIZATION,
+  apiKey: process.env.PAT,
 });
 
 const openai = new OpenAIApi(configuration);
 
 const generatePrompt = (hobby) => {
   // Generate the prompt based on the given hobby
-  return `Tell me about a random hobby: ${hobby}`;
+  return `Tell me about a random hobby: ${hobby} in 3 sentences please`;
 };
 
 module.exports = {
@@ -23,6 +23,7 @@ module.exports = {
         model: 'text-davinci-003',
         prompt: generatePrompt(req.body.interests),
         temperature: 0.6,
+        max_tokens: 200,
       });
       const response = completion.data.choices[0].text;
       console.log(response);
@@ -33,4 +34,3 @@ module.exports = {
     }
   }
 }
-
