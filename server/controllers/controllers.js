5cce89e77ac956cdd34752700f2856343abc0889
Jerry Renn <jerryrenn1998@gmail.com>
2023-06-01 22:03:04 -0700

    implement working postman post req for davinci

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -3,26 +3,34 @@ const { OpenAIApi, Configuration } = require('openai');
 
 const configuration = new Configuration({
   organization: 'org-JJSozU3BIZWcitrWaAKfRcbl',
-  apiKey: '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',
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
 
+const generatePrompt = (hobby) => {
+  // Generate the prompt based on the given hobby
+  return `Tell me about a random hobby: ${hobby}`;
+};
+
 module.exports = {
 
   getHobby: async (req, res) => {
+
+    console.log(req.body)
     try {
-      const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [
-          { role: 'user', content: 'hello world' }
-        ]
+      const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: generatePrompt(req.body.interests),
+        temperature: 0.6,
       });
-      console.log(completion.data.choices[0].message);
-      res.json({ completion: completion.data.choices[0].message });
+      const response = completion.data.choices[0].text;
+      console.log(response);
+      res.json({ result: response });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'An error occurred' });
     }
   }
 }
+
